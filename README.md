099bfbee1e68d8ed3dc178b91ccb42c09be1ed7f
Rodrigo Valeretto <rodrigoaugustovaleretto@gmail.com>
2023-05-02 20:41:06 -0300

    Updating anonymize proto - fixing response case and commenting file

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ It is important to install and configure GRPC tools for python, the documentatio
 
 To generate the GRPC files correctly use the command
 ```shell
-python3 -m grpc_tools.protoc --proto_path=protos --python_out=. --pyi_out=. --grpc_python_out=. protos/anonymize.proto
+python3 -m grpc_tools.protoc --proto_path=protos --python_out=. --pyi_out=. --grpc_python_out=. protos/*.proto
 ```
 
 To run the project, being in the root folder of it, run
--- anonymization.py
+++ anonymization.py
@@ -3,34 +3,30 @@ from concurrent import futures
 import time
 import anonymize_pb2_grpc as pb2_grpc
 import anonymize_pb2 as pb2
-from deepface import DeepFace
+
+# from deepface import DeepFace
 from PIL import Image
 import io
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 class AnonymizationService(pb2_grpc.AnonymizerServicer):
-
     def __init__(self, *args, **kwargs):
         pass
 
-    async def Anonymize(self, request, context):
-
+    def Anonymize(self, request, context):
         print("Request received:", request)
 
         image_bytes = request.image
 
         image = Image.open(io.BytesIO(image_bytes))
 
-        embedded_image = DeepFace.represent(
-            image.getdata(), model_name="Facenet512")
+        # embedded_image = DeepFace.represent(image)
 
-        print(embedded_image)
+        # print(embedded_image)
 
         result = pb2.AnonymizeRS()
 
-        result.anonymized_image = image_bytes
+        result.anonymizedImage = image_bytes
 
         print("Resultado ANONYMIZE_RS", result)
 
@@ -40,11 +36,11 @@ class AnonymizationService(pb2_grpc.AnonymizerServicer):
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     pb2_grpc.add_AnonymizerServicer_to_server(AnonymizationService(), server)
-    server.add_insecure_port('[::]:5098')
+    server.add_insecure_port("[::]:5098")
     server.start()
     print("Server Started - Awaiting requests...")
     server.wait_for_termination()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     serve()
